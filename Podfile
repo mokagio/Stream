74cbabdddf3bad2b75683b509ff108fd5510c3f7
Gio <giovanni.lodi42@gmail.com>
2014-07-31 09:07:11 +1000

    CocoaLumberjack setup with colored output support
    See https://github.com/CocoaLumberjack/CocoaLumberjack
    
    Colored output is awesome, I can't work without it.
    
    I rekon is important to set up the logging framework as the first step of the development because otherwise the cost of doing it increases with the number of NSLogs that are gonna be written in the meantime.

--- Podfile
+++ Podfile
@@ -1,7 +1,7 @@
 platform :ios, "7.0"
 
 target "Stream" do
-
+  pod "CocoaLumberjack", "~> 1.9.1"
 end
 
 target "StreamTests" do
--- Stream/STRAppDelegate.m
+++ Stream/STRAppDelegate.m
@@ -1,13 +1,36 @@
 #import "STRAppDelegate.h"
 
+// Power logging
+#import <DDTTYLogger.h>
+#import <DDASLLogger.h>
+
 @implementation STRAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+
+    [self setupDDLog];
+
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
+}
+
+#pragma mark - CocoaLumberjack Logging
+
+- (void)setupDDLog
+{
+    [DDLog addLogger:[DDASLLogger sharedInstance]];
+    [DDLog addLogger:[DDTTYLogger sharedInstance]];
+
+    DDTTYLogger *ttyLogger = [DDTTYLogger sharedInstance];
+    [ttyLogger setColorsEnabled:YES];
+    [ttyLogger setForegroundColor:[UIColor redColor] backgroundColor:nil forFlag:LOG_FLAG_ERROR];
+    [ttyLogger setForegroundColor:[UIColor orangeColor] backgroundColor:nil forFlag:LOG_FLAG_WARN];
+    [ttyLogger setForegroundColor:[UIColor yellowColor] backgroundColor:nil forFlag:LOG_FLAG_DEBUG];
+    [ttyLogger setForegroundColor:[UIColor greenColor] backgroundColor:nil forFlag:LOG_FLAG_INFO];
+    [ttyLogger setForegroundColor:[UIColor cyanColor] backgroundColor:nil forFlag:LOG_FLAG_VERBOSE];
 }
 
 @end
