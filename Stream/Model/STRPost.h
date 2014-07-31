0dac918c71d9528046303f31bc4c29399b3a9a7b
Gio <giovanni.lodi42@gmail.com>
2014-07-31 13:36:48 +1000

    Post model now has all the relevant properties

--- Stream/Model/STRPost.h
+++ Stream/Model/STRPost.h
@@ -3,5 +3,9 @@
 @interface STRPost : NSObject
 
 @property (nonatomic, copy) NSString *text;
+@property (nonatomic, copy) NSString *authorName;
+@property (nonatomic, copy) NSString *authorHandle;
+@property (nonatomic, strong) NSURL *authorAvatarURL;
+@property (nonatomic, copy) NSDate *createdDate;
 
 @end
