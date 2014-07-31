279babd70ec8c4ec34956c7f02e07d868b8d33c3
Gio <giovanni.lodi42@gmail.com>
2014-07-31 17:44:41 +1000

    Back button is now white :wink:

--- Stream/ViewControllers/NavigationController/STRNavigationController.m
+++ Stream/ViewControllers/NavigationController/STRNavigationController.m
@@ -14,6 +14,7 @@
                                                NSFontAttributeName: [UIFont openSansBoldFontOfSize:[UIFont labelFontSize]],
                                                NSForegroundColorAttributeName: [UIColor whiteColor],
                                                };
+    self.navigationBar.tintColor = [UIColor whiteColor];
 
     return self;
 }
