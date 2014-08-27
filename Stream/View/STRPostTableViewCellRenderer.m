2c8a35d5fa88697764d3c6831937833b2b8f1c58
Gio <giovanni.lodi42@gmail.com>
2014-08-27 18:36:14 +1000

    Use App.net proxy to download avatar, instead of AFNetworking

--- Stream/View/STRPostTableViewCellRenderer.m
+++ Stream/View/STRPostTableViewCellRenderer.m
@@ -1,19 +1,43 @@
 #import "STRPostTableViewCellRenderer.h"
 #import "STRPostTableViewCell.h"
 #import "STRPost.h"
-#import <UIImageView+AFNetworking.h>
+#import "STRAppDotNetProxy.h"
 #import <NSDate+DateTools.h>
 #import <UIFont+OpenSans.h>
 
+@interface STRPostTableViewCellRenderer ()
+
+@property (nonatomic, strong) STRAppDotNetProxy *appDotNetProxy;
+@property (nonatomic, strong) UIImage *placeHolderImage;
+
+@end
+
 @implementation STRPostTableViewCellRenderer
+
+- (id)init
+{
+    self = [super init];
+    if (!self) { return nil; }
+
+    self.appDotNetProxy = [STRAppDotNetProxy configuredProxy];
+    self.placeHolderImage = [UIImage imageNamed:@"AvatarPlaceholder"];
+
+    return self;
+}
 
 - (void)configureCell:(STRPostTableViewCell *)cell withPost:(STRPost *)post
 {
     [cell setAuthorName:post.authorName andHandle:post.authorHandle];
     cell.postTextLabel.text = post.text;
-    [cell.avatarImageView setImageWithURL:post.authorAvatarURL
-                         placeholderImage:[UIImage imageNamed:@"AvatarPlaceholder"]];
     cell.ageLabel.text = [post.createdDate shortTimeAgoSinceNow];
+    cell.avatarImageView.image = self.placeHolderImage;
+
+    [self.appDotNetProxy getAvatarImageForPost:post
+                              withSuccessBlock:^(UIImage *image) {
+                                  cell.avatarImageView.image = image;
+                              } failureBlock:^(NSError *error) {
+                                  DDLogError(@"Failed downloading avatar image. Error: %@", error.localizedDescription);
+                              }];
 }
 
 @end
